15f6097a2329ecb980afb5cc13f1a5952f84c6c2
Mikkel N. Broholm <mibr0005@stud.ek.dk>
2026-03-24 14:03:00 +0100

    Add basic input loop with quit command to Main class

--- src/com/atm/Main.java
+++ src/com/atm/Main.java
@@ -1,7 +1,19 @@
 package com.atm;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final String CMD_QUIT = "q";
+
     static void main() {
 
+        Scanner scanner = new Scanner(System.in);
+
+        boolean quit = false;
+
+        while(!quit) {
+            String input = scanner.nextLine();
+            if(input.equals("q")) return;
+        }
     }
 }
