0e72334e4ce2d47a441cfad650ef2e4f8aad460e
Isabellabmj <Isabellajoensen@gmail.com>
2026-03-24 14:17:35 +0100

    ICustomer and Customer done

---
+++ src/com/atm/customer/ICustomer.java
@@ -0,0 +1,11 @@
+package com.atm.customer;
+
+public interface ICustomer {
+
+    public String getName();
+    public String getSurname();
+
+    public void setName(String name);
+    public void setSurname(String surname);
+
+}
