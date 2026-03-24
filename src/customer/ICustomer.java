e98724e5fdeee5b0a972980b1b10f80df2a35c69
Isabellabmj <Isabellajoensen@gmail.com>
2026-03-24 14:01:32 +0100

    Customer and ICustomer done

---
+++ src/customer/ICustomer.java
@@ -0,0 +1,13 @@
+package customer;
+
+public interface ICustomer {
+
+    public String getName();
+    public String getLastName();
+
+    public void setName(String name);
+
+    public void setLastName(String lastname);
+
+    
+}
